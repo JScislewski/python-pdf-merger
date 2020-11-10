a9414d2e1f6eba24b8d2b641486167571e238b0d
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-10 23:49:48 +0100

    Add saving to output file

--- merger.py
+++ merger.py
@@ -1,15 +1,22 @@
 import PyPDF4, os
 
 files = []
+
 for file in os.listdir("."):
     if file.endswith(".pdf"):
         files.append(file)
+
 files.sort(key=str.lower)
 pdf_writer = PyPDF4.PdfFileWriter()
-print(files)
+
 for file in files:
     file_obj = open(file, "rb")
     pdf_reader = PyPDF4.PdfFileReader(file_obj)
-    for page in pdf_reader.numPages:
+
+    for page in range(0, pdf_reader.getNumPages()):
         page_obj = pdf_reader.getPage(page)
-        pdf_writer.add(page_obj)
+        pdf_writer.addPage(page_obj)
+
+output = open("merged.pdf", "wb")
+pdf_writer.write(output)
+output.close()
